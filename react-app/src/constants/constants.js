a430feb11a022e90a8c5265ea1bd6fac95b53ce3
Brandoneck <brandoneck@live.com.mx>
2020-12-02 05:21:19 -0600

    CRUD de tiempos y formulario

--- react-app/src/constants/constants.js
+++ react-app/src/constants/constants.js
@@ -1,3 +1,5 @@
 export const LOGIN = 'http://localhost:3001/login';
 export const USERS = 'http://localhost:3001/getUsers';
 export const CURSOS = 'http://localhost:3001/getCursos';
+export const TIEMPOS = 'http://localhost:3001/getTiempos';
+export const NEWTIMES = 'http://localhost:3001/addTiempos';
